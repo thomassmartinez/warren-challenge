3a5859ed9b2ea2356f27232e279ed6b089d78b97
thomas martinez <tmartinez@gvdasa.com.br>
2021-08-09 15:48:34 -0300

    fix: remove wait import

--- src/pages/Home/components/TransactionsList/TransactionsList.test.tsx
+++ src/pages/Home/components/TransactionsList/TransactionsList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {fireEvent, render, wait, waitFor} from '@testing-library/react';
+import {fireEvent, render, waitFor} from '@testing-library/react';
 
 import {TransactionsList} from '.';
 
